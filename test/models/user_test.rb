9021d7e6a913d540f20e940eba49e403c365048c
martensonbj <martensonbj@gmail.com>
2016-01-26 18:47:29 -0700

    User signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -53,6 +53,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test "email addresses should be saed as lower_case" do
+    mixed_case_email = "FoObAr@exAmPle.com"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
   test "password should be present" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
